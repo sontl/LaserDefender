d5dc73293a6dd561285efd4ddb9b3807bd0dba5b
Son Tran Lam <sontl@code.edu.vn>
2020-11-07 12:13:32 +0800

    Add new Boss enemy type with spinning bomb

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    [SerializeField] float speedOfSpinner = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 0f, speedOfSpinner));
+    }
+}
